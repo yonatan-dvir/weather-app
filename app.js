9ef369e16f4bc878873a4be9b2421d883b3de23a
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-08 12:35:57 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const request = require("request");
+
+const ACCESS_KEY = "c53dce3000c68a411951788245d6c1d9";
+
+const url = `http://api.weatherstack.com/current?access_key=c53dce3000c68a411951788245d6c1d9&query=New York`;
+
+request({ url: url, json: true }, (error, response) => {
+  data = response.body.current;
+  console.log(
+    `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out.`
+  );
+});
