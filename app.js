ba5441d46216d5cff1dd597bbe747a7f6d4b1783
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-08 14:00:40 +0300

    Implemented the requests for weather and location API's

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const request = require("request");
 
-const ACCESS_KEY = "c53dce3000c68a411951788245d6c1d9";
+const WEATHER_KEY = "c53dce3000c68a411951788245d6c1d9";
+const GEO_KEY =
+  "pk.eyJ1IjoieW9uYXRhbmR2aXIiLCJhIjoiY2xwbmF4a3NkMGo5dDJtdDN1dzVuaDY1OSJ9.yD9BaMV_sDYa1C-PtrVTmw";
 
-const url = `http://api.weatherstack.com/current?access_key=c53dce3000c68a411951788245d6c1d9&query=New York`;
+const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHER_KEY}&query=34.053691,-118.242766`;
 
-request({ url: url, json: true }, (error, response) => {
-  data = response.body.current;
-  console.log(
-    `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out.`
-  );
+request({ url: weatherURL, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to weather services!");
+  } else if (response.body.error) {
+    console.log("Unable to find location! Try another search.");
+  } else {
+    data = response.body.current;
+    console.log(
+      `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. Enjoy in ${response.body.location.name}!`
+    );
+  }
+});
+
+const geoURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${GEO_KEY}&limit=1`;
+
+request({ url: geoURL, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to location services!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location! Try another search.");
+  } else {
+    const longitude = response.body.features[0].center[0];
+    const latitude = response.body.features[0].center[1];
+    const location = response.body.features[0].text;
+    console.log(`You are in ${location} in GEO: ${latitude},${longitude}`);
+  }
 });
