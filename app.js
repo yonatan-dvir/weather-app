d43363a301ae134cbb81a011459bd9706e191bb8
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-08 15:29:00 +0300

    chained the requests so the weather errequests eill be according to the geo coordinates of the fiven location

--- app.js
+++ app.js
@@ -4,32 +4,43 @@ const WEATHER_KEY = "c53dce3000c68a411951788245d6c1d9";
 const GEO_KEY =
   "pk.eyJ1IjoieW9uYXRhbmR2aXIiLCJhIjoiY2xwbmF4a3NkMGo5dDJtdDN1dzVuaDY1OSJ9.yD9BaMV_sDYa1C-PtrVTmw";
 
-const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHER_KEY}&query=34.053691,-118.242766`;
+const geocode = (address, callback) => {
+  const geoURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=${GEO_KEY}&limit=1`;
+  let geoMessage = "";
+  request({ url: geoURL, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location! Try another search.", undefined);
+    } else {
+      const longitude = response.body.features[0].center[0];
+      const latitude = response.body.features[0].center[1];
+      callback(undefined, `${latitude},${longitude}`);
+    }
+  });
+};
 
-request({ url: weatherURL, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to weather services!");
-  } else if (response.body.error) {
-    console.log("Unable to find location! Try another search.");
-  } else {
-    data = response.body.current;
-    console.log(
-      `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. Enjoy in ${response.body.location.name}!`
-    );
-  }
-});
+const weather = (geos, callback) => {
+  const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHER_KEY}&query=${geos}`;
+  let weatherMessage = "t";
+  request({ url: weatherURL, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather services!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location! Try another search.", undefined);
+    } else {
+      data = response.body.current;
+      weatherMessage = `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. Enjoy in ${response.body.location.name}!`;
+      callback(undefined, weatherMessage);
+    }
+  });
+};
 
-const geoURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${GEO_KEY}&limit=1`;
-
-request({ url: geoURL, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to location services!");
-  } else if (response.body.features.length === 0) {
-    console.log("Unable to find location! Try another search.");
-  } else {
-    const longitude = response.body.features[0].center[0];
-    const latitude = response.body.features[0].center[1];
-    const location = response.body.features[0].text;
-    console.log(`You are in ${location} in GEO: ${latitude},${longitude}`);
-  }
+geocode("netanya", (error, data) => {
+  error ? console.log(error) : console.log(data);
+  weather(data, (error, data) => {
+    error ? console.log(error) : console.log(data);
+  });
 });
