4430efc4a1e639095f7cb3cbbf089125b321b6e1
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-09 18:05:39 +0300

    Started up a server with Express on port 3000

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const request = require("request");
-const geocode = require("./utils/geocode.js");
-const forecast = require("./utils/forecast.js");
+const express = require("express");
+// const geocode = require("./utils/geocode.js");
+// const forecast = require("./utils/forecast.js");
 
-geocode("boston", (error, data) => {
-  error ? console.log(error) : console.log(data);
-  forecast(data, (error, data) => {
-    error ? console.log(error) : console.log(data);
-  });
+// geocode("boston", (error, data) => {
+//   error ? console.log(error) : console.log(data);
+//   forecast(data, (error, data) => {
+//     error ? console.log(error) : console.log(data);
+//   });
+// });
+
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("You are in the home page");
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000...");
 });
