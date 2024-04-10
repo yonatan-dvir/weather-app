5292b3186490edf4956d9ad0c911d2a78cdc4aa2
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-10 10:58:28 +0300

    Created hbs views to make the web htmk documents dynamic

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log('Cliens Side JS is loaded!');
--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
+// node cores modules
 const path = require("path");
+// npm modules
 const request = require("request");
 const express = require("express");
 
@@ -17,6 +19,17 @@ const app = express();
 // Make the server serve up the public directory
 const publicDirPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirPath));
+
+// Setting hbs to be the view template engine
+app.set("view engine", "hbs");
+
+// Render the index view when accesing to root route
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Home Page - Weather App",
+    name: "Yonatan Dvir",
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000...");
